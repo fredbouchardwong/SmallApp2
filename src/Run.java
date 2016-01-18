e70889843b5d7215c7188736e5547c854d272496
fredbouchardwong <frederic.bouchard-wong@fredericwong.com>
2016-01-18 09:37:53 -0500

    Test for the first commit ever

---
+++ src/Run.java
@@ -0,0 +1,17 @@
+public class Run {
+
+    public static void main(String [ ] args)
+    {
+        System.out.println("ih paul");
+    }
+
+    //test
+
+    static void doThis () {
+//testeee
+    }
+
+}
+
+
+
